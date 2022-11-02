31a2bf25062aeec2f6df12315881869a29012690
Utkarsh Jha <ju.utkarshjha@gmail.com>
2022-11-02 12:44:00 +0530

    initializing project

---
+++ app/src/main/java/com/example/droidrush_null_pointer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.droidrush_null_pointer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
